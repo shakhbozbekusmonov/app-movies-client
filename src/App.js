2d5c59836fda6e4695b973c4da58de578c23100c
shakhbozbekusmonov <miracle_0319@bk.ru>
2022-02-06 20:50:48 +0500

    Project done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-
+import './sass/main.scss';
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
+import {Main} from "./layout/Main";
 
 function App() {
   return (
     <div className="App">
-
+      <Header/>
+        <Main/>
+      <Footer/>
     </div>
   );
 }
