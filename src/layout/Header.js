841d612db179971225c1e00e42f6571034d1544b
shakhbozbekusmonov <miracle_0319@bk.ru>
2022-02-10 10:12:25 +0500

    Project Debugged

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -23,7 +23,7 @@ export default function SearchAppBar() {
                         component="div"
                         sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' }, marginLeft: 100 }}
                     >
-                        WE HAVE THE BEST MOVIES
+                        BEST MOVIES
                     </Typography>
                 </Toolbar>
             </AppBar>
